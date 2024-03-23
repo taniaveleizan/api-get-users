826430e60bc446752e2f8cb935844452826e682f
Tania Veleizan <taninaveleizan@gmail.com>
2024-03-22 21:50:23 -0300

    Agregando funcion para filtrar usuarios por genero

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -6,7 +6,8 @@ const fetchRandomUsers = async(limit : number)=>{
     const response = await axios.get(`https://randomuser.me/api/?results=${limit}`);
     return response.data.results.map((user: any) =>({
         name: `${user.name.first} ${user.name.last}`,
-        email: user.email
+        email: user.email,
+        gender: user.gender
     }));
 }
 
@@ -16,6 +17,7 @@ let cachedUsers : any[] = [];
 export const getUsers = async (req: Request, res: Response) =>{
     try {
         const limit : number = parseInt(req.query.limit as string) || 10;
+        const categorize : string = req.query.categorize as string;
 
         //verificar si los users ya estan en cache y si la cantidad solicitada es menor o igual al tamaño de la cache
         if(cachedUsers.length >=limit){
@@ -24,11 +26,26 @@ export const getUsers = async (req: Request, res: Response) =>{
             //si no hay suficientes users en cache, obtener mas de la API randomuser.me
             const users = await fetchRandomUsers(limit);
             cachedUsers = users;
-            res.json({ cachedUsers });
+            res.json({ users : filterUsers(cachedUsers, categorize) });
         }
         
 
     } catch (error) {
         res.status(500).json({error: 'Internal server error'});
     }
+}
+
+//funcion para filtrar por genero
+function filterUsers(users: any[], categorizeBy: string){
+    const filteredUsers : any = {};
+
+    users.forEach(user =>{
+        if(!filteredUsers[user.gender]){
+            filteredUsers[user.gender]=[];
+        }
+
+        filteredUsers[user.gender].push(user);
+    });
+
+    return filteredUsers;
 }
