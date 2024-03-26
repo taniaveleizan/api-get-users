75491caeb59b442ce8baaa70103430108e6eb692
Tania Veleizan <taninaveleizan@gmail.com>
2024-03-25 21:43:59 -0300

    Agregando nuevo endpoint get users/drink

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -2,31 +2,36 @@ import { Request, Response } from "express";
 import axios from "axios";
 
 
-const fetchRandomUsers = async(limit : number)=>{
-    const response = await axios.get(`https://randomuser.me/api/?results=${limit}`);
-    return response.data.results.map((user: any) =>({
-        name: `${user.name.first} ${user.name.last}`,
-        email: user.email,
-        gender: user.gender
-    }));
+const fetchRandomUsers = async(limit : number): Promise<any[]>=>{
+    try {
+        const response = await axios.get(`https://randomuser.me/api/?results=${limit}`);
+        return response.data.results.map((user: any) =>({
+            name: `${user.name.first} ${user.name.last}`,
+            email: user.email,
+            gender: user.gender,
+            nacionalidad: user.nat
+        }));
+    } catch (error) {
+        throw new Error('Error fetching users from randomuser.me API');
+    }
 }
 
 let cachedUsers : any[] = [];
 
-
+//controller para /user con filtro por genero
 export const getUsers = async (req: Request, res: Response) =>{
     try {
         const limit : number = parseInt(req.query.limit as string) || 10;
-        const categorize : string = req.query.categorize as string;
+        const categorizeBy : string = req.query.categorize as string;
 
         //verificar si los users ya estan en cache y si la cantidad solicitada es menor o igual al tamaño de la cache
         if(cachedUsers.length >=limit){
-            res.json(cachedUsers.slice(0, limit));
+            res.json({ users: filterUsers(cachedUsers.slice(0, limit), categorizeBy)});
         }else{
             //si no hay suficientes users en cache, obtener mas de la API randomuser.me
             const users = await fetchRandomUsers(limit);
             cachedUsers = users;
-            res.json({ users : filterUsers(cachedUsers, categorize) });
+            res.json({ users : filterUsers(cachedUsers, categorizeBy) });
         }
         
 
@@ -36,16 +41,51 @@ export const getUsers = async (req: Request, res: Response) =>{
 }
 
 //funcion para filtrar por genero
-function filterUsers(users: any[], categorizeBy: string){
-    const filteredUsers : any = {};
+function filterUsers(users: any[], categorizeBy: string):any{
+    const filteredUsers : any = {
+        female: [],
+        male: []
+    };
 
     users.forEach(user =>{
-        if(!filteredUsers[user.gender]){
-            filteredUsers[user.gender]=[];
+        if(user.gender === 'female'){
+            filteredUsers.female.push(user);
+        }else if(user.gender === 'male'){
+            filteredUsers.male.push(user);
         }
 
-        filteredUsers[user.gender].push(user);
+        return filteredUsers;
     });
 
     return filteredUsers;
+}
+
+//Funcion para obtener drink aleatorio
+const getRandomDrink = async function(): Promise<string>{
+    try {
+        const response = await axios.get('hhtps://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const drinkName = response.data.drinks[0].strDrink;
+        return drinkName;
+    } catch (error) {
+        throw new Error('Error fetching random drink from thecocktaildb.com API');
+    }
+}
+
+//controller para users/drink
+export const getDrink = async (req: Request, res: Response) =>{
+    try {
+        const user = await fetchRandomUsers(1);
+        const cocktail = await getRandomDrink();
+
+        const userWithDrink = {
+            nombre: user[0].nombre,
+            email: user[0].email,
+            nacionalidad: user[0].nacionalidad,
+            'cocktail-favorito': cocktail
+        };
+
+        res.json({ user:[userWithDrink]});
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
 }
--- src/route/route.ts
+++ src/route/route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getUsers } from '../controller/users.controller';
+import { getDrink, getUsers } from '../controller/users.controller';
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+
+router.get('/users/drink', getDrink);
 
 export default router;
